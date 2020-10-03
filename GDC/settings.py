1844f92abdec15b2997f9633de31ea4f93ff4582
shrish <shrish.shukla23@gmail.com>
2020-10-03 17:26:30 +0530

    initial static

--- GDC/settings.py
+++ GDC/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/var/www/static/',
