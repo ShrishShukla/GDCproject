450fa3b575f8e72b03bb41e71c9993f3684cca09
shrish <shrish.shukla23@gmail.com>
2020-10-03 17:39:20 +0530

    initial settings

--- GDC/settings.py
+++ GDC/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('GDC_PROD_KEY')
+#SECRET_KEY = os.environ.get('GDC_PROD_KEY')
+
+SECRET_KEY = '&%d_zy)&m2mdylcgwh(i9e93h#p2n#xzndh7z=3yak37_7rzz9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
